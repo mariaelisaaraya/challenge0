15481f7efad5e21c7ae3ba1fe2972346c3317fc9
Maria Elisa Araya <42949801+mariaelisaaraya@users.noreply.github.com>
2024-11-23 20:57:05 -0300

    Update scaffold.config.ts

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -10,7 +10,19 @@ export type ScaffoldConfig = {
 
 const scaffoldConfig = {
   // The networks on which your DApp is live
-  targetNetworks: [chains.arbitrumSepolia],
+  targetNetworks: [
+    {
+      ...chains.sepolia,
+      rpcUrls: {
+        default: {
+          http: [`https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`],
+        },
+        public: {
+          http: [`https://eth-sepolia.g.alchemy.com/v2/${process.env.NEXT_PUBLIC_ALCHEMY_API_KEY}`],
+        },
+      },
+    },
+  ],
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect if you only target the local network (default is 4000)
